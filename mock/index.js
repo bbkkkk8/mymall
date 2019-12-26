209a223ac938d5f15f2d8afedf6fb0df21b91e5a
git <lixiaokang215@163.com>
2019-12-26 23:11:51 +0800

    动态路由未成功

--- mock/index.js
+++ mock/index.js
@@ -2,3 +2,4 @@ const Mock = require('mockjs')
 // 格式： Mock.mock( url, post/get , 返回的数据)；
 Mock.mock('/api/goodList', 'get', require('./json/goods'))
 // Mock.mock('/home/banner', 'get', require('./json/homeBanner'))
+Mock.mock('/api/routeData', 'get', require('./json/routeData'))
